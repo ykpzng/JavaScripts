2bb6c42a7fa4971d7d2ade67c14d79dc79570af3
Yakup ZENGİN <48925129+ykpzng@users.noreply.github.com>
2020-10-15 19:03:45 +0300

    Add array files

--- 8-Arrays/array.js
+++ 8-Arrays/array.js
@@ -1,14 +1,42 @@
 //Arrays
 
-let arrayList = ['Ali', 'Veli', 'Hasan', 23, 54];
-console.log(arrayList);
+const myAray = ['ali', 'veli', 34, 'deli', 76];
 
-for (let i = 0; i < arrayList.length; i++) {
-  console.log(arrayList[i]);
-}
+console.log(myAray);
 
-arrayList.forEach(element => {
-  console.log(element);
-});
+myAray[6] = 56;   // 5.index boş kalır 6.indexe eklenir
 
+console.log(myAray);
 
+myAray.push('haydar'); // Dizinin <sonuna> ekleme yapar
+console.log(myAray);
+
+myAray.unshift(178); // Dizinin <başına> ekleme yapar
+console.log(myAray);
+
+let silinen = myAray.pop(); // Dizinin <sonuncu> elemanını  siler. VE silinen elemanı geri döndürebilir
+console.log(myAray);
+console.log(silinen); //Pop ile silinen eleman
+
+silinen = myAray.shift(); // Dizinin <ilk> elemanını  siler. VE silinen elemanı geri döndürebilir
+console.log(myAray);
+console.log(silinen); //shift ile silinen eleman
+
+//-------------------------------------------
+
+const newArray = ['elma', 'armut', 'ayva', 'çilek'];
+console.log(newArray);
+
+silinen = newArray.splice(1, 2);  //Silinen 'armut', 'ayva'
+console.log(newArray);
+console.log(silinen); //splice ile silinen eleman
+
+newArray.splice(1, 0, 'şeftali');
+console.log(newArray);  //["elma", "şeftali", "çilek"]
+
+//-------------------------------------------
+//indexOf dizi içindeki elemanın index ini döner. Yoksa -1 döner
+console.log(newArray.indexOf('ayva')); // -1 döner (ayva listede yok)
+console.log(newArray.indexOf('çilek')); // 2 döner
+
+console.log(Object.keys(newArray)); // ["0", "1", "2"]
