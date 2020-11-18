6540fd09ed8dc3eed3ef4c42ea6a4567a184e566
Yakup ZENGİN <48925129+ykpzng@users.noreply.github.com>
2020-11-18 10:46:12 +0300

    Update 11.1-13.Day_Project

---
+++ 11.1-13.Day_Project/01-Drum Kit - Project/script.js
@@ -0,0 +1,41 @@
+
+//buttonlardan kontrol
+
+const drums = document.querySelectorAll('.drum');
+const order = ['w', 'a', 's', 'd', 'j', 'k', 'l'];
+
+for (let i = 0; i < drums.length; i++) {
+
+  drums[i].onclick = function () {
+    let audio = new Audio('sounds/tom-' + order[i] + '.mp3');
+    audio.play();
+  }
+
+}
+
+// Klavyeden kontrol
+
+document.onkeypress = function (e) {
+  let tmp;
+  if (e.code === 'KeyW') {
+    tmp = 'w';
+  } else if (e.code === 'KeyA') {
+    tmp = 'a';
+  } else if (e.code === 'KeyS') {
+    tmp = 's';
+  } else if (e.code === 'KeyD') {
+    tmp = 'd';
+  } else if (e.code === 'KeyJ') {
+    tmp = 'j';
+  } else if (e.code === 'KeyK') {
+    tmp = 'k';
+  } else if (e.code === 'KeyL') {
+    tmp = 'l';
+  }
+
+  let audio = new Audio('sounds/tom-' + tmp + '.mp3');
+  audio.play();
+}
+
+
+
