854cdaa0a105e5e5acaa6ddbfaddae5afa305226
Yakup ZENGİN <48925129+ykpzng@users.noreply.github.com>
2020-11-23 00:27:11 +0300

    Update Google Temrature V1.2, Hesaplama hataları düzeltildi

--- 10.2-Google-Temperature/script.js
+++ 10.2-Google-Temperature/script.js
@@ -7,97 +7,114 @@ let selectTwo = document.getElementById('select_two');
 
 // Birinci İnputbox değiştiğinde gerçekleşen işlemler
 inputOne.oninput = function () {
-  firstGroupChange();
+  if (inputOne.value) {
+    firstGroupChange();
+  } else {
+    inputTwo.value = '';
+  }
 
 }
 
 // Birinci select değeri değiştiğinde gerçekleşen işlemler
 document.getElementById('select_one').onchange = function () {
-  firstGroupChange();
-
+  if (inputOne.value) {
+    firstGroupChange();
+  } else {
+    inputTwo.value = '';
+  }
 }
 
 // İkinci İnputbox değiştiğinde gerçekleşen işlemler
 inputTwo.oninput = function () {
-  // inputOne.value = '';0
-  if (selectTwo.value === 'Celsius') {
-    celsius = inputTwo;
-    if (selectOne.value === 'Fahrenheit') {
-      inputOne.value = (Number(celsius.value * 9 / 5) + 32).toFixed(2);
+  if (inputTwo.value) {
+    if (selectTwo.value === 'Celsius') {
+      celsius = inputTwo;
+      if (selectOne.value === 'Fahrenheit') {
+        inputOne.value = (Number(celsius.value) * 9 / 5 + 32).toFixed(2);
+      }
+      if (selectOne.value === 'Kelvin') {
+        inputOne.value = (Number(celsius.value) + 273.15).toFixed(2);
+      }
+      if (selectOne.value === 'Celsius') {
+        inputOne.value = celsius.value;
+      }
     }
-    if (selectOne.value === 'Kelvin') {
-      inputOne.value = (Number(celsius.value + 273.15)).toFixed(2);
+    if (selectTwo.value === 'Fahrenheit') {
+      fahrenheit = inputTwo;
+      if (selectOne.value === 'Celsius') {
+        inputOne.value = (Number(fahrenheit.value) - 32 * 5 / 9).toFixed(2);
+      }
+      if (selectOne.value === 'Kelvin') {
+        inputOne.value = ((Number(fahrenheit.value) - 32) * 5 / 9 + 273.15).toFixed(2);
+      }
+      if (selectOne.value === 'Fahrenheit') {
+        inputOne.value = fahrenheit.value;
+      }
     }
-    if (selectOne.value === 'Celsius') {
-      inputOne.value = celsius.value;
+    if (selectTwo.value === 'Kelvin') {
+      kelvin = inputTwo;
+      if (selectOne.value === 'Celsius') {
+        inputOne.value = ((Number(kelvin.value) - 273.15)).toFixed(2);
+      }
+      if (selectOne.value === 'Fahrenheit') {
+        inputOne.value = ((Number(kelvin.value) - 273.15) * 9 / 5 + 32).toFixed(2);
+      }
+      if (selectOne.value === 'Kelvin') {
+        inputOne.value = kelvin.value;
+      }
     }
+  } else {
+    inputOne.value = '';
   }
-  if (selectTwo.value === 'Fahrenheit') {
-    fahrenheit = inputTwo;
-    if (selectOne.value === 'Celsius') {
-      inputOne.value = (Number(fahrenheit.value - 32) * 5 / 9).toFixed(2);
-    }
-    if (selectOne.value === 'Kelvin') {
-      inputOne.value = ((Number(fahrenheit.value) - 32) * 5 / 9 + 273.15).toFixed(2);
-    }
-    if (selectOne.value === 'Fahrenheit') {
-      inputOne.value = fahrenheit.value;
-    }
-  }
-  if (selectTwo.value === 'Kelvin') {
-    kelvin = inputTwo;
-    if (selectOne.value === 'Celsius') {
-      inputOne.value = ((Number(kelvin.value) - 273.15)).toFixed(2);
-    }
-    if (selectOne.value === 'Fahrenheit') {
-      inputOne.value = ((Number(kelvin.value) - 273.15) * 9 / 5 + 32).toFixed(2);
-    }
-    if (selectOne.value === 'Kelvin') {
-      inputOne.value = kelvin.value;
-    }
-  }
+
 }
 
 // İkinci select değeri değiştiğinde gerçekleşen işlemler
-
 document.getElementById('select_two').onchange = function () {
+
   inputTwo.value = '';
-  if (selectOne.value === 'Celsius') {
-    celsius = inputOne;
-    if (selectTwo.value === 'Fahrenheit') {
-      inputTwo.value = (Number(celsius.value * 9 / 5) + 32).toFixed(2);
+  if (inputOne.value) {
+    if (selectOne.value === 'Celsius') {
+      celsius = inputOne;
+      if (selectTwo.value === 'Fahrenheit') {
+        inputTwo.value = (Number(celsius.value) * 9 / 5 + 32).toFixed(2);
+      }
+      if (selectTwo.value === 'Kelvin') {
+        inputTwo.value = (Number(celsius.value) + 273.15).toFixed(2);
+      }
+      if (selectTwo.value === 'Celsius') {
+        inputTwo.value = celsius.value;
+      }
     }
-    if (selectTwo.value === 'Kelvin') {
-      inputTwo.value = (Number(celsius.value + 273.15)).toFixed(2);
+    if (selectOne.value === 'Fahrenheit') {
+      fahrenheit = inputOne;
+      if (selectTwo.value === 'Celsius') {
+        inputTwo.value = (Number(fahrenheit.value) - 32 * 5 / 9).toFixed(2);
+      }
+      if (selectTwo.value === 'Kelvin') {
+        inputTwo.value = ((Number(fahrenheit.value) - 32) * 5 / 9 + 273.15).toFixed(2);
+      }
+      if (selectTwo.value === 'Fahrenheit') {
+        inputTwo.value = fahrenheit.value;
+      }
     }
-    if (selectTwo.value === 'Celsius') {
-      inputTwo.value = celsius.value;
+    if (selectOne.value === 'Kelvin') {
+      kelvin = inputOne;
+      if (selectTwo.value === 'Celsius') {
+        inputTwo.value = ((Number(kelvin.value) - 273.15)).toFixed(2);
+      }
+      if (selectTwo.value === 'Fahrenheit') {
+        inputTwo.value = ((Number(kelvin.value) - 273.15) * 9 / 5 + 32).toFixed(2);
+      }
+      if (selectTwo.value === 'Kelvin') {
+        inputTwo.value = kelvin.value;
+      }
     }
+  } else {
+    inputTwo.value = '';
+
   }
-  if (selectOne.value === 'Fahrenheit') {
-    fahrenheit = inputOne;
-    if (selectTwo.value === 'Celsius') {
-      inputTwo.value = (Number(fahrenheit.value - 32) * 5 / 9).toFixed(2);
-    }
-    if (selectTwo.value === 'Kelvin') {
-      inputTwo.value = ((Number(fahrenheit.value) - 32) * 5 / 9 + 273.15).toFixed(2);
-    }
-    if (selectTwo.value === 'Fahrenheit') {
-      inputTwo.value = fahrenheit.value;
-    }
-  }
-  if (selectOne.value === 'Kelvin') {
-    kelvin = inputOne;
-    if (selectTwo.value === 'Celsius') {
-      inputTwo.value = ((Number(kelvin.value) - 273.15)).toFixed(2);
-    }
-    if (selectTwo.value === 'Fahrenheit') {
-      inputTwo.value = ((Number(kelvin.value) - 273.15) * 9 / 5 + 32).toFixed(2);
-    }
-    if (selectTwo.value === 'Kelvin') {
-      inputTwo.value = kelvin.value;
-    }
-  }
+
 
 }
 
@@ -107,10 +124,10 @@ function firstGroupChange() {
   if (selectOne.value === 'Celsius') {
     celsius = inputOne;
     if (selectTwo.value === 'Fahrenheit') {
-      inputTwo.value = (Number(celsius.value * 9 / 5) + 32).toFixed(2);
+      inputTwo.value = (Number(celsius.value) * 9 / 5 + 32).toFixed(2);
     }
     if (selectTwo.value === 'Kelvin') {
-      inputTwo.value = (Number(celsius.value + 273.15)).toFixed(2);
+      inputTwo.value = (Number(celsius.value) + 273.15).toFixed(2);
     }
     if (selectTwo.value === 'Celsius') {
       inputTwo.value = celsius.value;
