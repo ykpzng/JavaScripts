fbf56c8e9e03b3239dda739d71745bc3fe98e71b
Yakup ZENGİN <48925129+ykpzng@users.noreply.github.com>
2020-11-29 13:16:04 +0300

    Add Async-Await

---
+++ 13.03-Async_Await/script.js
@@ -0,0 +1,29 @@
+// Async - Await
+// Async function herzaman promise döndürür.
+
+async function name() {  // Function declaration
+  //await (pedding durumundaki işlem)
+}
+//OR
+const name1 = async () => { // Function expretion
+  //await  (pedding durumundaki işlem)
+}
+
+//----------------------------------------------
+
+const promiseFunction = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      resolve('It\'s resolved');
+      // reject('It\'s rejected');
+    }, 2000);
+  });
+}
+
+
+const asyncAwait = async () => {
+  const successMessage = await promiseFunction();
+  console.log(successMessage)
+}
+
+asyncAwait()
